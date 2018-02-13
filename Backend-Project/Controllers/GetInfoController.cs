4b6a5fe02ebb654e101cc34886c9a3e80e382b67
Antoine Roig <antoine.roig@hotmail.fr>
2018-02-13 21:45:05 +0100

    Models importés

---
+++ Backend-Project/Controllers/GetInfoController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Project.Controllers
+{
+    public class Person
+    {
+        public int age { get; set; }
+        public string nom { get; set; }
+    }
+
+    public class GetInfoController : ApiController
+    {
+        // GET: api/GetInfo
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GetInfo/5
+        public string Get(int id)
+        {
+
+            Person p = new Person();
+            p.age = id;
+            p.nom = "tg";
+            return (JsonConvert.SerializeObject(p));
+
+        }
+
+        // POST: api/GetInfo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/GetInfo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/GetInfo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
