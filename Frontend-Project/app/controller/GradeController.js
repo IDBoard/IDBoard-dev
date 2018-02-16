ea6fe70d852cab4b684f5a2a973e6f4ad8ed2b8d
Antuanett <antoinettebenzeno@gmail.com>
2018-02-16 12:53:16 +0100

    mise a jour
    service -- grades

---
+++ Frontend-Project/app/controller/GradeController.js
@@ -0,0 +1,7 @@
+﻿(function (app) {
+    var GradeController = function ($scope, apiGrades) {
+        $scope.message = "Grades";
+        
+    };
+    app.controller("GradeController", GradeController);
+}(angular.module("App")));
