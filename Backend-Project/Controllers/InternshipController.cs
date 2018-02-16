64546eedf6cb32bc2faf2424ab9ca0fd2dfee205
Apex <Apex@DESKTOP-12P76NP>
2018-02-16 17:47:51 +0100

    InternshipController : Renvoi la liste des stages

---
+++ Backend-Project/Controllers/InternshipController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Project.Controllers
+{
+    public class InternshipController : ApiController
+    {
+        // GET: api/Internship : Renvoie la liste des stages
+        public IHttpActionResult Get()
+        {
+            IdBoardDb iDBoard = new IdBoardDb();
+
+            var InternshipOffersList = (from t1 in iDBoard.InternshipOffers
+                                        join t2 in iDBoard.TypesInternships
+                                        on t1.idTypeInternship equals t2.idTypeInternship
+                                        select new
+                                        {
+                                            t1.Reference,
+                                            t1.Title,
+                                            t1.MissionSummary,
+                                            t1.CompanyName,
+                                            t1.Duration,
+                                            t1.DateStart,
+                                            t1.DateEnd,
+                                            t1.Contact,
+                                            t1.ContactPhone,
+                                            t1.ContactMail,
+                                            t1.Address1,
+                                            t1.Address2,
+                                            t1.PostalCode,
+                                            t1.City,
+                                            t1.Country,
+                                            t2.TypeDefaultValue,
+                                        }).ToList();
+
+            return Ok(JsonConvert.SerializeObject(InternshipOffersList, Formatting.Indented, new JsonSerializerSettings()
+            { ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore }
+            ));
+        }
+    }
+}
---
+++ Backend-Project/Controllers/SalutationController.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Project.Controllers
+{
+    public class SalutationController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            IdBoardDb iDBoard = new IdBoardDb();
+
+
+            var list = from salutation in iDBoard.Salutations select salutation.SalutationDefaultValue;
+            return Ok(JsonConvert.SerializeObject(list));
+        }
+    }
+}
