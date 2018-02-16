c3f0f49983b04cb62c6d94685667cc0c5f3cd4bd
Anthony Fromy <atacikia@gmail.com>
2018-02-16 15:33:54 +0100

    Main template

---
+++ Frontend-Project/js/controllers/Connection.controller.js
@@ -0,0 +1,13 @@
+﻿app.controller('Connection', function ($scope) {
+
+    $scope.idboard_number = 2011644;
+    $scope.password = '2669NICOLAS2107';
+    $scope.logs = '';
+    $scope.click = function () {
+        if ($scope.idboard_number !== '' && $scope.password !== '') {
+            alert('envoyé !!');
+            $scope.logs = $scope.idboard_number + ' ' + $scope.password;
+            $scope.color = 'green';
+        }
+    }
+});
