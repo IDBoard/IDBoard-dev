e5fc66eeae658c3e18214dd0d9715e87a4364f58
Antoine Roig <antoine.roig@hotmail.fr>
2018-02-16 17:42:35 +0100

    Recupération plusieurs noms élèves = GetTrombiController

--- Backend-Project/Controllers/GetTrombiController.cs
+++ Backend-Project/Controllers/GetTrombiController.cs
@@ -11,12 +11,25 @@ namespace Backend_Project.Controllers
 
     public class GetTrombiController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(int id)
         {
             IDBoardDb idBoard = new IDBoardDb();
 
-            var listStudent = from eleves in idBoard.BusinessEntities select eleves.Name;
-            return Ok(JsonConvert.SerializeObject(listStudent));
+//          var listStudent = from eleves in idBoard.BusinessEntities select eleves;
+            var listId = from idBE in idBoard.JoinEntityClass where (idBE.idClass == id) select idBE.idBusinessEntity;
+            List<int> listStudent = new List<int>();
+            foreach (var IDB in listId)
+            {
+                //var Student = from eleve in idBoard.BusinessEntities where eleve.idBusinessEntity == IDB select eleve.idBusinessEntity;
+                //listStudent.Add(Student);
+                listStudent.Add((from eleve in idBoard.BusinessEntities where eleve.idBusinessEntity == IDB select eleve.idBusinessEntity).FirstOrDefault());
+            }
+            List<string> listS = new List<string>();
+            foreach (var lS in listStudent)
+            {
+                listS.Add((from eleve in idBoard.BusinessEntities where eleve.idBusinessEntity == lS select eleve.Name).FirstOrDefault());
+            }
+            return Ok(JsonConvert.SerializeObject(listS));
         }
     }
 }
