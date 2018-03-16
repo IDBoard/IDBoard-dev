bfec3a54e8ea70253ec8e453f334cf3a8fea810f
Antoine Roig <antoine.roig@hotmail.fr>
2018-03-16 14:31:40 +0100

    Controller get class, model class

---
+++ Backend-Project/Models/ClassesIdboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend_Project.Models
+{
+    public class ClassesIdboard
+    {
+        public int idClass { get; set; }
+        public String Name { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+}
