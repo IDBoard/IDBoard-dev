935c898226b8a0c61999fde6b303494006d34dbf
nicolachoquet06250 <nicolachoquet06250@gmail.com>
2018-03-16 14:24:34 +0100

    Rectification des erreurs sur le trombinoscope

--- Frontend-Project/js/controllers/Trombinoscope.controller.js
+++ Frontend-Project/js/controllers/Trombinoscope.controller.js
@@ -4,8 +4,8 @@ idboard.controller('Trombinoscope', function ($scope, $http) {
         $scope.id_classe = 5;
     };
 
-    /*$scope.get_trombi = function () {
-        $http.get('http://localhost:4725/api/GetTrombi/' + $scope.id_classe)
+    $scope.get_trombi = function () {
+        /*$http.get('http://localhost:4725/api/GetTrombi/' + $scope.id_classe)
             .then(function(data) {
                 console.log(data);
             },
@@ -16,60 +16,53 @@ idboard.controller('Trombinoscope', function ($scope, $http) {
 
         data = $scope.genere_json_test();
 
-        data.students = [];
-        for (var i = 0, max = data.nameStudent.length; i < max; i++) {
-            data.students[i] = {
-                id: data.idStudent[i],
-                firstname: data.firstnameStudent[i],
-                lastname: data.nameStudent[i],
-                picture: data.pictureStudent[i]
-            }
-        }
-
-        delete data.idStudent;
-        delete data.nameStudent;
-        delete data.firstnameStudent;
-
         $scope.class_name = data.nameClasse;
         $scope.nbr_etudiants = data.nbStudent;
-        $scope.etudiants = data.students;
+        $scope.etudiants = data.listStudent;
     };
 
     $scope.genere_json_test = function () {
         return {
             nbStudent: 6,
             nameClasse: 'M1 SDEAM Sophia Antipolis 2017/2018',
-            nameStudent: [
-                'CHOQUET',
-                'CARRIE',
-                'GIUNTI',
-                'GODART',
-                'VAUTHIER',
-                'LEON'
-            ],
-            firstnameStudent: [
-                'Nicolas',
-                'Stéphanie',
-                'Hugo',
-                'Killian',
-                'Stéphane',
-                'Valentin'
-            ],
-            idStudent: [
-                2011644,
-                2013342,
-                2011602,
-                2011455,
-                2011662,
-                2013393
-            ],
-            pictureStudent: [
-                '/Uploads/2011644/photo_Choquet%20Nicolas%20-%202011644.jpg',
-                '/Uploads/2013342/photo_Stéphanie%20CARRIE.jpg',
-                '/Uploads/2011602/photo_GIUNTI%20Hugo%20-%202011602.jpg',
-                '/Uploads/2011455/photo_Killian%20Godart%20-%202011455.jpg',
-                '/Uploads/2011662/photo_Vauthier%20Stéphane.jpg',
-                '/Uploads/2013393/photo_LEON.png'
+            listStudent: [
+                {
+                    id: 2011644,
+                    name: "CHOQUET",
+                    firstname: "Nicolas",
+                    picture: "/Uploads/2011644/photo_Choquet%20Nicolas%20-%202011644.jpg"
+
+                },
+                {
+                    id: 2013342,
+                    name: "CARRIE",
+                    firstname: "Stéphanie",
+                    picture: "/Uploads/2013342/photo_Stéphanie%20CARRIE.jpg"
+                },
+                {
+                    id: 2011602,
+                    name: "GIUNTI",
+                    firstname: "Hugo",
+                    picture: "/Uploads/2011602/photo_GIUNTI%20Hugo%20-%202011602.jpg"
+                },
+                {
+                    id: 2011455,
+                    name: "GODART",
+                    firstname: "Killian",
+                    picture: "/Uploads/2011455/photo_Killian%20Godart%20-%202011455.jpg"
+                },
+                {
+                    id: 2011662,
+                    name: "VAUTHIER",
+                    firstname: "Stéphane",
+                    picture: "/Uploads/2011662/photo_Vauthier%20Stéphane.jpg"
+                },
+                {
+                    id: 2013393,
+                    name: "LEON",
+                    firstname: "Valentin",
+                    picture: "/Uploads/2013393/photo_LEON.png"
+                }
             ]
         }
     };
