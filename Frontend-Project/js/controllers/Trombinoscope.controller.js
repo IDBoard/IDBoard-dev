c6f8436b255aeaa863ec6935d123406718d7cd30
nicolachoquet06250 <nicolachoquet06250@gmail.com>
2018-03-16 17:32:26 +0100

    Modification pour ne pas afficher les élèves inactifs

--- Frontend-Project/js/controllers/Trombinoscope.controller.js
+++ Frontend-Project/js/controllers/Trombinoscope.controller.js
@@ -4,6 +4,16 @@ idboard.controller('Trombinoscope', function ($scope, $http) {
         $scope.id_classe = 5;
     };
 
+    $scope.delete_unactive_students = function (students) {
+        for (let i = 0, max = students.length; i < max; i++) {
+            if (!students[i].active) {
+                delete students[i];
+            }
+        }
+
+        return students;
+    };
+
     $scope.get_trombi = function () {
         /*$http.get('http://localhost:4725/api/GetTrombi/' + $scope.id_classe)
             .then(function(data) {
@@ -15,6 +25,8 @@ idboard.controller('Trombinoscope', function ($scope, $http) {
             });*/
 
         data = $scope.genere_json_test();
+
+        data.listStudent = $scope.delete_unactive_students(data.listStudent);
 
         $scope.class_name = data.nameClasse;
         $scope.nbr_etudiants = data.nbStudent;
@@ -30,38 +42,51 @@ idboard.controller('Trombinoscope', function ($scope, $http) {
                     id: 2011644,
                     name: "CHOQUET",
                     firstname: "Nicolas",
-                    picture: "/Uploads/2011644/photo_Choquet%20Nicolas%20-%202011644.jpg"
+                    picture: "/Uploads/2011644/photo_Choquet%20Nicolas%20-%202011644.jpg",
+                    active: true
 
                 },
                 {
                     id: 2013342,
                     name: "CARRIE",
                     firstname: "Stéphanie",
-                    picture: "/Uploads/2013342/photo_Stéphanie%20CARRIE.jpg"
+                    picture: "/Uploads/2013342/photo_Stéphanie%20CARRIE.jpg",
+                    active: true
                 },
                 {
                     id: 2011602,
                     name: "GIUNTI",
                     firstname: "Hugo",
-                    picture: "/Uploads/2011602/photo_GIUNTI%20Hugo%20-%202011602.jpg"
+                    picture: "/Uploads/2011602/photo_GIUNTI%20Hugo%20-%202011602.jpg",
+                    active: true
                 },
                 {
                     id: 2011455,
                     name: "GODART",
                     firstname: "Killian",
-                    picture: "/Uploads/2011455/photo_Killian%20Godart%20-%202011455.jpg"
+                    picture: "/Uploads/2011455/photo_Killian%20Godart%20-%202011455.jpg",
+                    active: true
                 },
                 {
                     id: 2011662,
                     name: "VAUTHIER",
                     firstname: "Stéphane",
-                    picture: "/Uploads/2011662/photo_Vauthier%20Stéphane.jpg"
+                    picture: "/Uploads/2011662/photo_Vauthier%20Stéphane.jpg",
+                    active: true
                 },
                 {
                     id: 2013393,
                     name: "LEON",
                     firstname: "Valentin",
-                    picture: "/Uploads/2013393/photo_LEON.png"
+                    picture: "/Uploads/2013393/photo_LEON.png",
+                    active: true
+                },
+                {
+                    id: 2013310,
+                    name: "TOTO",
+                    firstname: "TATA",
+                    picture: "/Uploads/2013393/photo_LEON.png",
+                    active: false
                 }
             ]
         }
