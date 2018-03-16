12945fddf7ee91cca1f78a9930f5083711caee91
Antoine Roig <antoine.roig@hotmail.fr>
2018-03-16 14:16:53 +0100

    getmessage

--- Backend-Project/Controllers/GetMessagesController.cs
+++ Backend-Project/Controllers/GetMessagesController.cs
@@ -15,7 +15,8 @@ namespace Backend_Project.Controllers
             public string Title { get; set; }
             public string BBCODE { get; set; }
             public DateTime DateStart { get; set; }
-            public DateTime DateEnd { get; set; }
+          //  public DateTime DateEnd { get; set; } Ajouter fausse date de fin à la création pour éviter des soucis de variable null
+
             public Boolean Priority { get; set; }
             public Boolean AdminRequ { get; set; }
             public Boolean ModifReq { get; set; }
@@ -37,10 +38,10 @@ namespace Backend_Project.Controllers
                 message.Title = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.Title).FirstOrDefault();
                 message.BBCODE = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.Title).FirstOrDefault();
                 message.DateStart = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.DateStart).FirstOrDefault();
-                message.DateEnd = DateTime.Now;
-                message.DateEnd = (from msg in idBoard.Messages where (msg.idMessage == idmsg && msg.DateEnd != null) select msg.DateEnd).FirstOrDefault() ;
-                if (message.DateEnd.Equals(null))
-                    message.DateEnd = DateTime.Now;
+               // message.DateEnd = DateTime.Now;
+          //      message.DateEnd = (from msg in idBoard.Messages where (msg.idMessage == idmsg && msg.DateEnd != null) select msg.DateEnd).FirstOrDefault() ;
+           //     if (message.DateEnd.Equals(null))
+            //        message.DateEnd = DateTime.Now;
                 message.Priority = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.Priority).FirstOrDefault();
                 message.AdminRequ = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.AdministrativeRequest).FirstOrDefault();
                 message.ModifReq = (from msg in idBoard.Messages where msg.idMessage == idmsg select msg.ModificationRequest).FirstOrDefault();
