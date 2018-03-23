88bc49224afdb796e9e0f0eb1b5c1601ec5a626a
Antoine Roig <antoine.roig@hotmail.fr>
2018-03-23 15:46:00 +0100

    identity dependances

---
+++ Backend-Project/Controllers/EventController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend_Project.Controllers
+{
+    public class EventController : ApiController
+    {
+        public class Event
+        {
+            public int IdEvent { get; set; }
+            public int IdBusinessEntity { get; set; }
+            public int IdTypeEvent { get; set; }
+            public int IdSystemLevel { get; set; }
+            public int BBCode { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok();
+        }
+    }
+}
