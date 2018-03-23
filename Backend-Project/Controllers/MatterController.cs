8539d98e0371a699724fb5bcae4e8e160aeef236
Apex <Apex@DESKTOP-12P76NP>
2018-03-23 14:27:38 +0100

    AddMatter function

--- Backend-Project/Controllers/MatterController.cs
+++ Backend-Project/Controllers/MatterController.cs
@@ -11,11 +11,11 @@ namespace Backend_Project.Controllers
     public class MatterController : ApiController
     {
         //Get modules list from classe name (ex : /Matter/B3)
-        public IHttpActionResult Get(String ClasseName)
+        public IHttpActionResult Get(String classeName)
         {
             IdBoardDb iDBoard = new IdBoardDb();
 
-            int idClasse = (from Classes in iDBoard.Classes where Classes.Name == ClasseName select Classes.idClass).FirstOrDefault(); ;
+            int idClasse = (from Classes in iDBoard.Classes where Classes.Name == classeName select Classes.idClass).FirstOrDefault(); ;
 
             var MatterList = (from Matters in iDBoard.Matters where Matters.idClass == idClasse select Matters).ToList();
 
@@ -36,6 +36,30 @@ namespace Backend_Project.Controllers
 
             iDBoard.SaveChanges();
             
+            return Ok();
+        }
+
+        //Create a matter for a classe
+        [HttpPost]
+        public IHttpActionResult AddMatter(String _reference, String _description, int _marksNumber, double _credits, String _classeName)
+        {
+            IdBoardDb iDBoard = new IdBoardDb();
+
+            int _idClasse = (from Classes in iDBoard.Classes where Classes.Name == _classeName select Classes.idClass).FirstOrDefault(); ;
+
+            Matters newMatterQuery = new Matters
+            {
+                idClass = _idClasse,
+                Reference = _reference,
+                DescriptionDefaultValue = _description,
+                MarksNumber = _marksNumber,
+                ECTSCredits = _credits
+            };
+
+            iDBoard.Matters.Add(newMatterQuery);
+
+            iDBoard.SaveChanges();
+
             return Ok();
         }
 
