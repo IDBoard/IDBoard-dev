3887233f6e6ccb9cf8fba2fe40714f8f74500dbb
Apex <Apex@DESKTOP-12P76NP>
2018-06-08 02:01:14 +0200

    MarksController

---
+++ Backend-Project/Controllers/MarksController.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+
+
+namespace Backend_Project.Controllers
+{
+    public class MarksController : ApiController
+    {
+        // Push marks for a specific classe
+        //
+        //***************************************************************************************
+        // Format du json qui contient les informations des notes à passer en parametre
+        // 
+        // api/Marks/PushMarks?_idClass=777&_idMatter=166&_MarksInfos={'marks':[{'idBusinessEntity':8,'markCoef':1,'mark':14,'markComment':'bien', 'IsJustifiedAbsence':true},{'idBusinessEntity':9,'markCoef':1,'mark':10,'markComment':'passable','IsJustifiedAbsence':true},{'idBusinessEntity':10,'markCoef':1,'mark':17,'markComment':'très bien','IsJustifiedAbsence':true}]}
+        //
+        //***************************************************************************************
+
+        //!\\ Attention //!\\ La fonction ne peux marché que si : - L'ID matter existe en DB
+        //                                                        - Les ID type evaluation existes en DB
+        //                                                        - Les ID businessEntity existes en DB
+        //                                                        - L'ID class existe en DB
+
+        [HttpPost]
+        public IHttpActionResult PushMarks(int _idClass, int _idMatter, string _MarksInfos) // _MarksInfos est ensuite sérialisé en json (voir le format au dessus)
+        {
+            IdBoardDb iDBoard = new IdBoardDb();
+
+            var jsonMarksInfos = JsonConvert.DeserializeObject<dynamic>(_MarksInfos);
+
+            foreach (var markInfos in jsonMarksInfos.marks) // On boucle sur chaque eleve pour remplir une mark par eleve
+            {
+                Marks newMarkQuery = new Marks
+                {
+                    idMark = 0,
+                    idBusinessEntity = markInfos.idBusinessEntity,
+                    idMatter = _idMatter,
+                    idClass = _idClass,
+                    idTypeEvaluation = 1,
+                    Coefficient = markInfos.markCoef,
+                    Value = markInfos.mark,
+                    Comments = markInfos.markComment,
+                    DateCreation = DateTime.Today,
+                    IsJustifiedAbsence = markInfos.IsJustifiedAbsence
+                };
+
+                iDBoard.Marks.Add(newMarkQuery);
+                iDBoard.SaveChanges();
+
+            }
+            
+            return Ok();
+        }
+    }
+}
--- Backend-Project/Controllers/PersonController.cs
+++ Backend-Project/Controllers/PersonController.cs
@@ -70,6 +70,63 @@ namespace Backend_Project.Controllers
             return Ok(JsonConvert.SerializeObject(PersonDetails));
         }
 
+        // POST: AddPerson => _type == (Homme || Femme || Morale)
+        [HttpPost]
+        public IHttpActionResult UpdatePerson(int _idBusinessEntity, String _type, int _idSystemLevel, int _IDBoard, String _firstName, String _name, String _surName, String _photoPath, 
+            String _dateOfBirth, String _placeOfBirth, String _nationality, String _contryOfBirth, String _comment, String _adress1, String _adresse2, int _postalCode,
+            String _city, String _country)
+        {
+            IdBoardDb iDBoard = new IdBoardDb();
+
+            var PersonDetails = (from person in iDBoard.BusinessEntities
+                                 where person.idBusinessEntity == _idBusinessEntity
+                                 join typebusinnesentity in iDBoard.TypesBusinessEntities
+                                 on person.idTypeBusinessEntity equals typebusinnesentity.idTypeBusinessEntity
+                                 join fichecontact in iDBoard.ContactDetails
+                                 on person.idBusinessEntity equals fichecontact.idBusinessEntity
+                                 where fichecontact.DateEnd == null
+                                 // on prend la fiche contact que si date end == null pour prendre la plus recente
+                                 select new
+                                 {
+                                     person.idBusinessEntity,
+                                     // person.idTypeBusinessEntity, => On selectionne le Type (en string) dans la table TypeBusinessEntity
+                                     // plutot que renvoyer un id
+                                     typebusinnesentity.TypeDefaultValue,
+                                     person.idSystemLevel,
+                                     person.IDBoard,
+                                     person.FirstName,
+                                     person.Name,
+                                     person.SurName,
+                                     person.PhotoPath,
+                                     person.DateOfBirth,
+                                     person.PlaceOfBirth,
+                                     person.Nationality,
+                                     person.CountryOfBirth,
+                                     person.Comments,
+                                     person.CardTokenID,
+                                     fichecontact.Address1,
+                                     fichecontact.Address2,
+                                     fichecontact.PostalCode,
+                                     fichecontact.City,
+                                     fichecontact.Country
+                                 }).ToList();
+
+
+         /*   Matters newMatterQuery = new Matters
+            {
+                idClass = _idClasse,
+                Reference = _reference,
+                DescriptionDefaultValue = _description,
+                MarksNumber = _marksNumber,
+                ECTSCredits = _credits
+            };
+            
+            iDBoard.Matters.Add(newMatterQuery);
+            */
+            iDBoard.SaveChanges();
+
+            return Ok();
+        }
 
     }
 }
